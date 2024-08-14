4c4014578bd472865a43dd1769b82be0032307fd
Cotangente00 <julianavila458@gmail.com>
2024-08-14 16:41:43 -0500

    Ahora el programa puede leer y procesar archivos en formato .xlsx sin problema alguno, nueva librería añadida, nueva función para convertir archivos y archivo con extensiones recomendadas

---
+++ convert_xlsx_to_xls.py
@@ -0,0 +1,27 @@
+import openpyxl
+from manipular_Hoja1 import *
+from manipular_INFORME_SOLICITUDES import *
+import xlwt
+
+
+
+def convert_xlsx_to_xls(xlsx_file, xls_file):
+    # Cargar el archivo .xlsx usando openpyxl
+    wb_xlsx = openpyxl.load_workbook(xlsx_file)
+   
+    # Crear un nuevo archivo .xls usando xlwt
+    wb_xls = xlwt.Workbook()
+
+    # Iterar sobre todas las hojas del archivo .xlsx
+    for sheet_name in wb_xlsx.sheetnames:
+        sheet_xlsx = wb_xlsx[sheet_name]
+        sheet_xls = wb_xls.add_sheet(sheet_name)
+
+        # Copiar contenido de cada hoja del .xlsx a .xls
+        for row_index, row in enumerate(sheet_xlsx.iter_rows()):
+            for col_index, cell in enumerate(row):
+                sheet_xls.write(row_index, col_index, cell.value)
+
+    wb_xls.save(xls_file)
+
+
--- test.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-from manipular_Hoja1 import *
-
-wb = openpyxl.load_workbook('test.xlsx')
-ws = wb['Hoja1']
-
-find_table_and_move_to_A5(ws)
-
-wb.save('result.xlsx')
