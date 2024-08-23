b4122d36deb5ff6b2619e9e3c8f24a98f8b0feb9
Cotangente00 <julianavila458@gmail.com>
2024-08-23 12:17:04 -0500

    Ajuste muy importante para que la aplicación resalte correctamente todas las expertas con novedades.

--- test.py
+++ test.py
@@ -2,28 +2,22 @@ import openpyxl
 from manipular_INFORME_SOLICITUDES import *
 from manipular_Hoja1 import *
 from openpyxl import *
+from funciones_weekend import *
+
 # Cargar el archivo de Excel y seleccionar la hoja
 
 wb = openpyxl.load_workbook('test.xlsx')
 ws = wb[wb.sheetnames[0]]
 
-delete_filas(ws)
-delete_ciudades_columnas(ws)
-date_format(ws)
-styles_columnSize(ws)
-int_format(ws)
+ejecucion_funciones(ws)
+
 
 ws = wb[wb.sheetnames[1]]
 ws2 = wb[wb.sheetnames[0]]
 
-concatenar_nombres_apellidos(ws)
-delete_columns(ws)
-move_data_to_D5(ws)
-encontrar_y_mover_coincidencias_cedulas_y_nombres(ws,ws2)
-organizar_tabla_alfabeticamente(ws2)
-encontrar_y_mover_coincidencias_nombres(ws,ws2)
-no_service_copypaste(ws2,ws)
-novedades_expertas(ws2)
+ejecucion_funciones2_viernes_sabado(ws,ws2)
+
+
 
 
 wb.save('result.xlsx')
