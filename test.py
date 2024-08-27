1be709a291acd0b05bc15891b7951f9891c64b1c
Cotangente00 <julianavila458@gmail.com>
2024-08-27 10:23:19 -0500

    ajuste

--- test.py
+++ test.py
@@ -7,17 +7,20 @@ from funciones_weekend import *
 # Cargar el archivo de Excel y seleccionar la hoja
 
 wb = openpyxl.load_workbook('test.xlsx')
-ws = wb[wb.sheetnames[0]]
+#ws = wb[wb.sheetnames[0]]
 
-ejecucion_funciones(ws)
+#ejecucion_funciones(ws)
 
 
-ws = wb[wb.sheetnames[1]]
+#ws = wb[wb.sheetnames[1]]
 ws2 = wb[wb.sheetnames[0]]
 
-ejecucion_funciones2_viernes_sabado(ws,ws2)
+#ejecucion_funciones2_viernes_sabado(ws,ws2)
 
-
+for cell in column:
+    if cell.value:
+      cell.font = cell.font.copy(bold=True)
+      cell.style = 'Currency'  # Aplicar formato de moneda para subrayado
 
 
 wb.save('result.xlsx')
